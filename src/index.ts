bae6be2bb44478724ec82d2dd0f32b287ff9553f
Jeswin <jeswinpk@agilehead.com>
2020-11-17 11:09:59 +0530

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,18 @@
-import { FileChange, Repo } from "./types";
+import { ChangeFunc, FileChange, GitstaProvider } from "@gitsta/types";
 
-export type ChangeFunc = (change: FileChange) => boolean;
+let provider: GitstaProvider;
 
-export function setup() {
-  
+export async function setup(provider: GitstaProvider) {}
+
+export async function subscribe(onChange: ChangeFunc) {
+  return await provider.subscribe(onChange);
 }
 
-export async function subscribe(repo: Repo, onChange: ChangeFunc) {
-
+export async function unsubscribe(onChange: ChangeFunc) {
+  return await provider.unsubscribe(onChange);
 }
 
-export async function unsubscribe(repo: Repo, fn: ChangeFunc) {
-
+export async function commit(repo: string, files: FileChange[]) {
+  return await provider.commit(repo, files);
 }
 
-export async function getRepos(name: string): Promise<Repo> {
-  return { name };
-}
-
-export async function getCommits(repo: Repo): Promise<CommitLogEntry[]> {
-  return [];
-}
-
-export async function getCommitDetails(id: string): Promise<Commit> {
-  return {
-    id: "somehash",
-    author: "jeswin",
-    time: "100",
-    files: [],
-  };
-}
-
-export async function createCommit(commit: CommitData) {}
-
-export async function getFiles() {}
-
-export async function createFile() {}
-
-export async function readFile() {}
-
-export async function updateFile() {}
-
-export async function deleteFile() {}
